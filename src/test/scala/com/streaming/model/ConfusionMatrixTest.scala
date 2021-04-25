f4578cabe787cb51df46b15a0410ad990517f937
Spyros <21125056+SpyrosKou@users.noreply.github.com>
2021-04-25 14:21:40 +0300

    Tested adding/removing observations

--- src/test/scala/com/streaming/model/ConfusionMatrixTest.scala
+++ src/test/scala/com/streaming/model/ConfusionMatrixTest.scala
@@ -28,4 +28,37 @@ class ConfusionMatrixTest extends AnyFlatSpec with should.Matchers {
     confusionMatrix.predictions.get("A").get("C") should be(3L)
 
   }
+
+  "A Confusion Matrix " should "be updated correctly with Observations" in {
+    val observationA = new Observation("A", Map("A" -> 124L, "B" -> 12L))
+    val observationB = new Observation("B", Map("A" -> 11L, "B" -> 321L))
+
+    val confusionMatrix = new ConfusionMatrix()
+    confusionMatrix.predictions.size should be(0)
+
+    val confusionMatrixA = confusionMatrix.addPrediction(observationA);
+    confusionMatrixA.predictions.size should be(1)
+    confusionMatrixA.predictions.get("A").get.size should be(2)
+    confusionMatrixA.predictions.get("A").get("A") should be(124L)
+    confusionMatrixA.predictions.get("A").get("B") should be(12L)
+
+    val confusionMatrixB = confusionMatrixA.addPrediction(observationB);
+    confusionMatrixB.predictions.size should be(2)
+    confusionMatrixB.predictions.get("B").get.size should be(2)
+
+    confusionMatrixB.predictions.get("A").get("A") should be(124L)
+    confusionMatrixB.predictions.get("A").get("B") should be(12L)
+    confusionMatrixB.predictions.get("B").get("A") should be(11L)
+    confusionMatrixB.predictions.get("B").get("B") should be(321L)
+
+    val confusionMatrixC = confusionMatrixB.removePrediction(observationB);
+    confusionMatrixC.predictions.size should be(1)
+    confusionMatrixC.predictions.get("A").get.size should be(2)
+    confusionMatrixC.predictions.get("A").get("A") should be(124L)
+    confusionMatrixC.predictions.get("A").get("B") should be(12L)
+
+
+    val confusionMatrixD = confusionMatrixC.removePrediction(observationB);
+    confusionMatrixD.predictions.size should be(0)
+  }
 }
