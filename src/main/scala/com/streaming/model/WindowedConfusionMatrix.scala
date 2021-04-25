887fd5c5bbf56d419e03b3d1bd7159816a22a0d9
Spyros <21125056+SpyrosKou@users.noreply.github.com>
2021-04-25 17:49:17 +0300

    Created WindowedConfusionMatrix and Test

---
+++ src/main/scala/com/streaming/model/WindowedConfusionMatrix.scala
@@ -0,0 +1,96 @@
+/**
+ * StreamingConfusionMatrixCalculator
+ *     Parallel calculation of the confusion matrix for a window of streamed label prediction written in Scala
+ *     https://github.com/SpyrosKou/StreamingConfusionMatrix
+ *     Copyright (C) 2021  Spyros Koukas
+ *
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.streaming.model
+
+import scala.collection.immutable.Queue
+
+
+/**
+ * A ConfusionMatrix that
+ * - will maintain window size constant and valid by removing old values in a FIFO way
+ * - Can fit any number of different labels
+ * Created at 2021-04-25 on 11:42 
+ *
+ * @author Spyros Koukas
+ */
+final class WindowedConfusionMatrix(val confusionMatrix: ConfusionMatrix, val windowPredictions: Int, val observations: Queue[Observation]) {
+
+  def this(windowPredictions: Int) {
+    this(new ConfusionMatrix(), windowPredictions, Queue.empty)
+  }
+
+  /**
+   * The observation window is full when the observations are equal to the required windowPredictions
+   *
+   * @return
+   */
+  final def isWindowFull(): Boolean = {
+    this.observations.length == this.windowPredictions
+  }
+
+
+  /**
+   *
+   * @param observation
+   * @return
+   */
+  final def add(observation: Observation): WindowedConfusionMatrix = {
+
+    //create a new queue that includes new observation
+    var updatedQueue = this.observations.enqueue(observation)
+    //create a new matrix, updated with new observation, this is a mutable reference
+    var matrix = this.confusionMatrix.addPrediction(observation)
+
+    //Iterate until the queue is in the required window size.
+    // Should normally be executed once, but it may be handy for future functionalities.
+    while (updatedQueue.length > this.windowPredictions) {
+      val dequeue: (Observation, Queue[Observation]) = updatedQueue.dequeue
+      //get the rest of the queue
+      updatedQueue = dequeue._2
+      //Remove from matrix
+      matrix = matrix.removePrediction(dequeue._1)
+    }
+
+    return new WindowedConfusionMatrix(matrix, this.windowPredictions, updatedQueue)
+  }
+
+  /**
+   *
+   * @param actualLabel
+   * @param resultLabel
+   * @return
+   */
+  final def occurrences(actualLabel: String, resultLabel: String): Long = {
+    val observationsForLabel = occurrences(actualLabel)
+    return observationsForLabel.getOrElse(resultLabel, 0)
+  }
+
+  /**
+   *
+   * @param actualLabel
+   * @return
+   */
+  final def occurrences(actualLabel: String): Map[String, Long] = {
+    return this.confusionMatrix.predictions.getOrElse(actualLabel, Map.empty)
+
+  }
+
+}
---
+++ src/test/scala/com/streaming/model/WindowedConfusionMatrixTest.scala
@@ -0,0 +1,103 @@
+/**
+ * StreamingConfusionMatrixCalculator
+ * Parallel calculation of the confusion matrix for a window of streamed label prediction written in Scala
+ * https://github.com/SpyrosKou/StreamingConfusionMatrix
+ * Copyright (C) 2021  Spyros Koukas
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.streaming.model
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should
+
+/**
+ * Created at 2021-04-25 on 14:31 
+ *
+ * @author Spyros Koukas - koukasspyros@gmail.com
+ */
+class WindowedConfusionMatrixTest extends AnyFlatSpec with should.Matchers {
+
+  "A WindowedConfusionMatrix" should "be trivial to construct" in {
+    val windowedConfusionMatrix = new WindowedConfusionMatrix(3)
+    windowedConfusionMatrix.isWindowFull() should be(false)
+
+  }
+
+  "A WindowedConfusionMatrix" should "respect window limits" in {
+    val observationA = new Observation("A", Map("A" -> 124L, "B" -> 12L))
+    val observationB = new Observation("B", Map("A" -> 11L, "B" -> 321L))
+    val observationC = new Observation("B", Map("A" -> 41L, "B" -> 231L))
+    val observationD = new Observation("A", Map("A" -> 1224L, "B" -> 122L))
+
+    //0
+    val windowedConfusionMatrixA = new WindowedConfusionMatrix(3)
+    windowedConfusionMatrixA.isWindowFull() should be(false)
+    windowedConfusionMatrixA.occurrences("A","A") should be (0L)
+    windowedConfusionMatrixA.occurrences("A","B") should be (0L)
+    windowedConfusionMatrixA.occurrences("B","A") should be (0L)
+    windowedConfusionMatrixA.occurrences("B","B") should be (0L)
+    windowedConfusionMatrixA.occurrences("C","A") should be (0L)
+    windowedConfusionMatrixA.occurrences("C","C") should be (0L)
+
+    //1
+    val windowedConfusionMatrixB = windowedConfusionMatrixA.add(observationA)
+    windowedConfusionMatrixB.isWindowFull() should be(false)
+    windowedConfusionMatrixB.occurrences("A","A") should be (124L)
+    windowedConfusionMatrixB.occurrences("A","B") should be (12L)
+    windowedConfusionMatrixB.occurrences("B","A") should be (0L)
+    windowedConfusionMatrixB.occurrences("B","B") should be (0L)
+    windowedConfusionMatrixB.occurrences("C","A") should be (0L)
+    windowedConfusionMatrixB.occurrences("C","C") should be (0L)
+
+
+
+    //2
+    val windowedConfusionMatrixC = windowedConfusionMatrixB.add(observationB)
+    windowedConfusionMatrixC.isWindowFull() should be(false)
+    windowedConfusionMatrixC.occurrences("A","A") should be (124L)
+    windowedConfusionMatrixC.occurrences("A","B") should be (12L)
+    windowedConfusionMatrixC.occurrences("B","A") should be (11L)
+    windowedConfusionMatrixC.occurrences("B","B") should be (321L)
+    windowedConfusionMatrixC.occurrences("C","A") should be (0L)
+    windowedConfusionMatrixC.occurrences("C","C") should be (0L)
+
+
+    //3
+    val windowedConfusionMatrixD = windowedConfusionMatrixC.add(observationC)
+    windowedConfusionMatrixD.isWindowFull() should be(true)
+    windowedConfusionMatrixD.occurrences("A","A") should be (124L)
+    windowedConfusionMatrixD.occurrences("A","B") should be (12L)
+    windowedConfusionMatrixD.occurrences("B","A") should be (52L)
+    windowedConfusionMatrixD.occurrences("B","B") should be (552L)
+    windowedConfusionMatrixD.occurrences("C","A") should be (0L)
+    windowedConfusionMatrixD.occurrences("C","C") should be (0L)
+
+
+
+
+    //4
+    val windowedConfusionMatrixE = windowedConfusionMatrixD.add(observationD)
+    windowedConfusionMatrixE.isWindowFull() should be(true)
+    windowedConfusionMatrixE.occurrences("A","A") should be (1224L)
+    windowedConfusionMatrixE.occurrences("A","B") should be (122L)
+    windowedConfusionMatrixE.occurrences("B","A") should be (52L)
+    windowedConfusionMatrixE.occurrences("B","B") should be (552L)
+    windowedConfusionMatrixE.occurrences("C","A") should be (0L)
+    windowedConfusionMatrixE.occurrences("C","C") should be (0L)
+
+  }
+
+}
