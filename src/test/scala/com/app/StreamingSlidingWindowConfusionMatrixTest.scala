53674d478ba4d72dd00e271f88a1abd23aa4fbdc
Spyros <21125056+SpyrosKou@users.noreply.github.com>
2021-04-28 02:39:38 +0300

    Created test placeholder

---
+++ src/test/scala/com/app/StreamingSlidingWindowConfusionMatrixTest.scala
@@ -0,0 +1,34 @@
+/**
+ * StreamingConfusionMatrixCalculator
+ *     Parallel calculation of the confusion matrix for a window of streamed label prediction written in Scala
+ *     https://github.com/SpyrosKou/StreamingConfusionMatrix
+ *     Copyright (C) 2021  Spyros Koukas
+ *
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.app
+
+import org.scalatest.flatspec._
+import org.scalatest.matchers._
+
+/**
+ * Test the application
+ *
+ * @author Spyros Koukas
+ */
+class StreamingSlidingWindowConfusionMatrixTest extends AnyFlatSpec with should.Matchers {
+
+
+}
