6a6845dee620dd938107933cb4c0d088137a81b7
Spyros <21125056+SpyrosKou@users.noreply.github.com>
2021-04-28 02:30:13 +0300

    Added functionality

--- src/main/scala/com/app/StreamingSlidingWindowConfusionMatrix.scala
+++ src/main/scala/com/app/StreamingSlidingWindowConfusionMatrix.scala
@@ -1,9 +1,16 @@
 package com.app
 
+import akka.actor.ActorSystem
+import com.elasticsearch.query.{JsonParser, PersistenceAccess}
 import com.streaming.model.Configuration.ElasticSearchClient._
+import com.streaming.model.Configuration.{ElasticSearchClient, SUB_STREAMS}
+import com.streaming.model.{Configuration, WindowedConfusionMatrix}
 import org.apache.http.HttpHost
 import org.elasticsearch.client.indices.GetIndexRequest
 import org.elasticsearch.client.{RequestOptions, RestClient, RestHighLevelClient}
+
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
 
 final object StreamingSlidingWindowConfusionMatrix {
   final def main(args: Array[String]) = {
@@ -14,7 +21,9 @@ final object StreamingSlidingWindowConfusionMatrix {
     println(OUTPUT_INDEX_NAME + " index exists: " + appInstance.indexExists(OUTPUT_INDEX_NAME))
 
     //Calculates Streaming instances.
-    appInstance.calculate()
+    val countFuture = appInstance.calculate()
+    //Wait until test is done
+    val counter = Await.result(countFuture, Duration.Inf)
 
     appInstance.close()
   }
@@ -28,10 +37,53 @@ final class StreamingSlidingWindowConfusionMatrix {
 
   /**
    *
-   * @return the number of inputs read, outputs processed,millis duration
+   * @return a future that returns the number of inputs processed when the process is complete
    */
-  final def calculate(): (Long, Long, Long) = {
-    (1L, 1L, 1L)
+  final def calculate(): Future[Long] = {
+    implicit val system: ActorSystem = ActorSystem("Test")
+
+    val weights = Configuration.WEIGHTS_MAP
+
+    val client = new RestHighLevelClient(RestClient.builder(new HttpHost(ElasticSearchClient.IP, ElasticSearchClient.PORT, ElasticSearchClient.SCHEME)))
+    val jsonParser: JsonParser = new JsonParser();
+    val persistenceAccess = new PersistenceAccess(client, ElasticSearchClient.INPUT_INDEX_NAME)
+    val timeStart = System.currentTimeMillis()
+    val countFuture: Future[Long] = persistenceAccess.getAllInputsSource().
+      async.
+      //and run them on windowed confusion matrix
+      map(modelsPredictionProbabilities => modelsPredictionProbabilities.observation(weights)).
+
+      //and run them on windowed confusion matrix
+      scan((new WindowedConfusionMatrix(Configuration.WINDOW_SIZE), 0))((windowTuple, observation) => (windowTuple._1.add(observation), windowTuple._2 + 1)).
+
+      //filter only full windows
+      filter(windowTuple => windowTuple._1.isWindowFull()).
+      async.
+      groupBy(SUB_STREAMS, tuple => tuple._2 % SUB_STREAMS).
+      map(x => x._1).
+      map(windowTuple => windowTuple.confusionMatrix).
+      map(confusionMatrixTuple => jsonParser.toJsonXContentBuilder(confusionMatrixTuple)).
+      //count successes
+      map(json => persistenceAccess.put(json, Configuration.ElasticSearchClient.OUTPUT_INDEX_NAME)).
+      async.
+      map(result => if (result) 1L else 0L).
+
+      mergeSubstreams.
+      //      map(result => 1L).
+      //count by reduction
+      runReduce((x, y) => x + y)
+
+
+    implicit val ec = system.dispatcher
+
+    countFuture.onComplete(totalValues => {
+      //terminate actor system
+      system.terminate()
+    })
+
+
+    return countFuture
+
   }
 
   /**
